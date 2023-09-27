4e6dfa30c78fe037e701a5107ec7227973401ba2
Merlijn <merlijnbrinks@gmail.com>
2023-09-27 12:36:09 +0200

    init

---
+++ CSV-trainer.Core/ModelTrainer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSV_trainer.Core
+{
+    public class ModelTrainer
+    {
+        public string GetResult()
+        {
+            return "Hello, World!";
+        }
+    }
+}
